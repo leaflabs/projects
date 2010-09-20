411e5f03cafaac96dfbbeac7e5f700ca315185c1
Marti Bolivar <mbolivar@mit.edu>
2010-09-20 18:05:19 -0400

    pong works

--- vga/main.cpp
+++ vga/main.cpp
@@ -8,7 +8,7 @@
 
 #include "wirish.h"
 
-#define LED_PIN 13
+//----------------------- VGA defines/types/prototypes ------------------------
 
 // Pinouts -- you also must change the GPIO macros below if you change
 // these
@@ -18,7 +18,8 @@
 #define VGA_V 11    // STM32: A6
 #define VGA_H 12    // STM32: A7
 
-// These low level macros make GPIO writes much faster
+// These low level (and STM32 specific) macros make GPIO writes much
+// faster
 #define ABSRR ((volatile uint32*)0x40010810)
 #define ABRR  ((volatile uint32*)0x40010814)
 
@@ -47,26 +48,45 @@
 #define VGA_H_HIGH *ABSRR = BIT(7)
 #define VGA_H_LOW  *ABRR  = BIT(7)
 
-// when in the 0-2286 cycle range we start drawing the board
+// when in the 0-2286 horizontal line cycle range we start drawing the board
 #define START_IMAGE 400
-
-// joystick pins
-#define LEFT_UP 20
-#define LEFT_DOWN 19
-#define RIGHT_UP 17
-#define RIGHT_DOWN 16
 
 // controls the board size
 #define BOARD_WIDTH 54
 #define BOARD_HEIGHT 48
 
-// board is 1-bit, this controls the color of off/on
-#define ON_COLOR BBIT
-#define OFF_COLOR GBIT
+// board color scheme
+#define BORDER_COLOR RBIT  // color of the bands around the game board
+#define ON_COLOR BBIT      // default "filled" color
+#define OFF_COLOR GBIT     // default "empty" color
 
-// paddle IDs
-#define LEFT_PADDLE 0
-#define RIGHT_PADDLE 1
+void isr_porch(void);
+void isr_start(void);
+void isr_update(void);
+
+// this is (hackishly) used to force delays
+volatile uint8 volatile_int = 0;
+
+// vga state
+uint16 x = 0;       // X coordinate
+uint16 y = 0;       // Y coordinate
+uint8 v_active = 1; // Are we in the image?
+
+// each board cell is an OR of RBIT, GBIT, and BBIT
+//
+// access like board[y][x], where board[0][0] is upper left, and
+// board[BOARD_HEIGHT-1][BOARD_WIDTH-1] is bottom right
+uint8 board[BOARD_HEIGHT][BOARD_WIDTH];
+uint8 *board_row;    // cache our current row at hsync time, for speed
+
+//----------------------- Pong defines/types/prototypes -----------------------
+
+// joystick pins
+#define LEFT_UP 20
+#define LEFT_DOWN 19
+#define RIGHT_UP 17
+#define RIGHT_DOWN 15
+
 // paddle x positions, in board coordinates
 #define LEFT_PADDLE_X 0
 #define RIGHT_PADDLE_X (BOARD_WIDTH-1)
@@ -76,56 +96,58 @@
 #define PADDLE_HEIGHT 7
 // controls how fast the ball moves
 #define BALL_OVERFLOW 3
+// ball color when a goal is scored
+#define GOAL_SCORED_COLOR RBIT
 
 // joystick commands -- we only read up and down
 typedef enum {
     C_UP, C_DOWN, C_NONE
 } command_t;
 
-// vga functions
-void isr_porch(void);
-void isr_start(void);
-void isr_update(void);
+// paddle data structure
+typedef struct _paddle {
+    int8 x;                     // board x-coord
+    int8 y;                     // board y-coord
+    command_t cmd;              // last joystick command
+    uint8 count;                // # repeats of last joystick command
+    uint8 up_pin;
+    uint8 down_pin;
+} paddle_t;
+
+// ball data structure
+typedef struct _ball {
+    int8 x;
+    int8 y;
+    int8 vx;
+    int8 vy;
+    int8 count;
+} ball_t;
 
 // board update functions
 void advance_state();
-void update_paddle(uint8, uint8, uint8, uint8*, command_t*, uint8*);
-void clear_paddle(uint8);
-void draw_paddle(uint8);
-void update_ball();
-void clear_ball();
-void draw_ball();
-
-// this is (hackishly) used to force delays
-volatile uint8 volatile_int = 0;
-
-// vga state
-uint8 toggle;
-uint16 x = 0;       // X coordinate
-uint16 y = 0;       // Y coordinate
-uint8 v_active = 1; // Are we in the image?
+void update_paddle(paddle_t*);
+void clear_paddle(paddle_t*);
+void draw_paddle(paddle_t*);
+void update_ball(ball_t*);
+void clear_ball(ball_t*);
+void draw_ball(ball_t*);
+boolean is_blocking(int8, int8, int8, int8);
 
 // board state
-uint8 pad_left = 6;          // board y-coord of left paddle bottom
-uint8 pad_right = 6;         // likewise
-command_t left_cmd = C_NONE;  // last left command
-command_t right_cmd = C_NONE; // last right command
-uint8 pad_left_count = 0;     // number of times had last left command
-uint8 pad_right_count = 0;    // likewise
-int8 ball_x = 8, ball_y = 8;
-int8 ball_vx = 1, ball_vy = 1;
-uint8 ball_count = 0;
-
-// 1-bit!
-//
-// access like board[y][x], where board[0][0] is upper left, and
-// board[BOARD_HEIGHT-1][BOARD_WIDTH-1] is bottom right
-uint8 board[BOARD_HEIGHT][BOARD_WIDTH];
-uint8 *board_row;               // for speed
+paddle_t left_paddle = { /* .x = */ LEFT_PADDLE_X, /* .y = */ 6,
+                         /* .cmd = */ C_NONE, /* .count = */ 0,
+                         /* .up_pin = */ LEFT_UP, /* down_pin = */ LEFT_DOWN };
+paddle_t right_paddle = { /* .x = */ RIGHT_PADDLE_X, /* .y = */ 6,
+                          /* .cmd = */ C_NONE, /* .count = */ 0,
+                          /* .up_pin = */RIGHT_UP,/* down_pin = */ RIGHT_DOWN};
+ball_t ball = { /* .x = */ BOARD_WIDTH / 2, /* .y = */ BOARD_HEIGHT / 2,
+                /* .vx = */1, /* .vy = */ 1,
+                /* .count = */ 0 };
 
 //------------------------------ setup()/loop() -------------------------------
 
-void setup_board() { // board cells with 1 become blue, 0 become green
+// true board cells become ON_COLOR, false become OFF_COLOR
+void setup_board() {
     for (int y = 0; y < BOARD_HEIGHT; y++) {
         for (int x = 0; x < BOARD_WIDTH; x++) {
             if (board[y][x]) {
@@ -139,7 +161,6 @@ void setup_board() { // board cells with 1 become blue, 0 become green
 
 void setup() {
     // Setup our pins
-    pinMode(LED_PIN, OUTPUT);
     pinMode(VGA_R, OUTPUT);
     pinMode(VGA_G, OUTPUT);
     pinMode(VGA_B, OUTPUT);
@@ -175,12 +196,8 @@ void setup() {
     Timer4.attachCompare1Interrupt(isr_porch);
     Timer4.setCompare2(START_IMAGE);
     Timer4.attachCompare2Interrupt(isr_start);
-    // (we used to turn things black here, but now isr_start does it).
-    // if isr_start stops doing that, you may need to put this back in.
-    // Timer4.setCompare3(2170);
-    // Timer4.attachCompare3Interrupt(isr_stop);
-    Timer4.setCompare4(1);      // Could be zero I guess
-    Timer4.attachCompare4Interrupt(isr_update);
+    Timer4.setCompare3(1);      // Could be zero I guess
+    Timer4.attachCompare3Interrupt(isr_update);
 
     Timer4.setCount(0);         // Ready...
     Timer4.resume();            // Go!
@@ -195,28 +212,29 @@ void setup() {
 void isr_porch(void) {
     VGA_H_HIGH;
     y++;
-    board_row = board[y/(480/BOARD_HEIGHT)];
+    board_row = board[y / (480/BOARD_HEIGHT)];
     // Back to the top
-    if(y>=523) {
-        y=1;
+    if(y >= 523) {
+        y = 1;
         v_active = 1;
+        board_row = board[0];
         return;
     }
     // Other vsync stuff below the image
-    if(y>=492) {
+    if(y >= 492) {
         VGA_V_HIGH;
         return;
     }
-    if(y>=490) {
+    if(y >= 490) {
         VGA_V_LOW;
         return;
     }
-    if(y==479) {
+    if(y == 479) {
         v_active = 0;           // stop drawing
         return;
     }
     // give lines 480--489 to game logic
-    if(y>=480) {
+    if(y >= 480) {
         advance_state();
         return;
     }
@@ -227,9 +245,9 @@ void isr_start(void) {
     // Skip if we're not in the image at all
     if(!v_active) { return; }
 
-    // Start red.  Overhead setting up the main loop is going to make
-    // this appear as a wide band.
-    VGA_RED;
+    // Start with the border.  Overhead setting up the main loop is
+    // going to make this appear as a wide band.
+    VGA_BIT(BORDER_COLOR);
 
     // main loop: draw the contents of the board as fast as we can.
     // use VGA_BIT so per-iteration time is deterministic.
@@ -240,8 +258,8 @@ void isr_start(void) {
     // delay just a little so the last board pixel doesn't look so narrow
     volatile_int++;
 
-    // add another red band to mark off the edge
-    VGA_RED;
+    // add another band to mark off the edge
+    VGA_BIT(BORDER_COLOR);
     volatile_int++;
     volatile_int++;
     volatile_int++;
@@ -261,20 +279,18 @@ void isr_update(void) {
 //-------------------------------- Game logic ---------------------------------
 
 // the interrupt handlers will give lines 480--489 to this function,
-// which must finish its work before isr_stop on
+// which must finish its work before isr_stop on each line (which is
+// why we only update one thing (ball, paddle) at a time
 void advance_state() {
     switch (y) {
     case 480:
-        update_ball();
+        update_ball(&ball);
         break;
     case 481:
-        update_paddle(LEFT_PADDLE, LEFT_UP, LEFT_DOWN,
-                      &pad_left, &left_cmd, &pad_left_count);
-
+        update_paddle(&left_paddle);
         break;
     case 482:
-        update_paddle(RIGHT_PADDLE, RIGHT_UP, RIGHT_DOWN,
-                      &pad_right, &right_cmd, &pad_right_count);
+        update_paddle(&right_paddle);
         break;
     default:
         break;
@@ -282,118 +298,128 @@ void advance_state() {
     return;
 }
 
-void update_ball() {
-    clear_ball();
-    ball_count++;
-    if (ball_count >= BALL_OVERFLOW) {
-        ball_count = 0;
-        if (ball_vx > 0) {
-            if (ball_x + ball_vx < BOARD_WIDTH) {
-                ball_x += ball_vx;
-            } else {
-                ball_x = BOARD_WIDTH-1;
-                ball_vx *= -1;
+void update_ball(ball_t *ball) {
+    clear_ball(ball);
+    (ball->count)++;
+
+    if (ball->count >= BALL_OVERFLOW) {
+        ball->count = 0;
+        int8 next_x = ball->x + ball->vx, next_y = ball->y + ball->vy;
+        boolean blocked_left = is_blocking(left_paddle.x, left_paddle.y,
+                                           next_x, next_y);
+        boolean blocked_right = is_blocking(right_paddle.x, right_paddle.y,
+                                            next_x, next_y);
+
+        // update x-coord
+        if (ball->vx > 0) {
+            if (blocked_right) {
+                ball->x = right_paddle.x - 1;
+                ball->vx *= -1;
+            } else if (next_x < BOARD_WIDTH) {
+                ball->x = next_x;
+            } else {            // GOL! GOOOL GOOOL GOOOOOL!
+                ball->x = BOARD_WIDTH-1;
+                ball->vx *= -1;
+                // make the ball stay where it is for a few overflow
+                // periods so you can tell a goal was scored
+                ball->count = - 4 * BALL_OVERFLOW;
             }
         } else {
-            if (ball_x + ball_vx > 0) {
-                ball_x += ball_vx;
-            } else {
-                ball_x = 0;
-                ball_vx *= -1;
+            if (blocked_left) {
+                ball->x = left_paddle.x + 1;
+                ball->vx *= -1;
+            } else if (next_x > 0) {
+                ball->x = next_x;
+            } else {            // GOL! GOOOL GOOOL GOOOOOL!
+                ball->x = 0;
+                ball->vx *= -1;
+                ball->count = - 4 * BALL_OVERFLOW;
             }
         }
-        if (ball_vy > 0) {
-            if (ball_y + ball_vy < BOARD_HEIGHT) {
-                ball_y += ball_vy;
+
+        // update y-coord
+        if (!(blocked_left || blocked_right)) {
+            if (ball->vy > 0) {
+                if (next_y < BOARD_HEIGHT) {
+                    ball->y = next_y;
+                } else {
+                    ball->y = BOARD_HEIGHT-1;
+                    ball->vy *= -1;
+                }
             } else {
-                ball_y = BOARD_HEIGHT-1;
-                ball_vy *= -1;
-            }
-        } else {
-            if (ball_y + ball_vy > 0) {
-                ball_y += ball_vy;
-            } else {
-                ball_y = 0;
-                ball_vy *= -1;
+                if (next_y > 0) {
+                    ball->y = next_y;
+                } else {
+                    ball->y = 0;
+                    ball->vy *= -1;
+                }
             }
         }
     }
-    draw_ball();
+
+    draw_ball(ball);
 }
 
-inline void clear_ball() {
-    board[ball_y][ball_x] = OFF_COLOR;
+inline void clear_ball(ball_t *ball) {
+    board[ball->y][ball->x] = OFF_COLOR;
 }
 
-inline void draw_ball() {
-    board[ball_y][ball_x] = ON_COLOR;
+inline void draw_ball(ball_t *ball) {
+    if (ball->x == 0 || ball->x == BOARD_WIDTH - 1) {
+        board[ball->y][ball->x] = GOAL_SCORED_COLOR;
+    } else {
+        board[ball->y][ball->x] = ON_COLOR;
+    }
 }
 
-void update_paddle(uint8 paddle, uint8 up_pin, uint8 down_pin,
-                   uint8 *pos, command_t *cmd, uint8 *count) {
+inline boolean is_blocking(int8 paddle_x, int8 paddle_y, int8 x, int8 y) {
+    return x == paddle_x && paddle_y <= y && y < paddle_y + PADDLE_HEIGHT;
+}
+
+void update_paddle(paddle_t *paddle) {
     clear_paddle(paddle);
 
-    if (digitalRead(up_pin)) {
-        if (*cmd == C_UP) {
-            (*count)++;
+    if (digitalRead(paddle->up_pin)) {
+        if (paddle->cmd == C_UP) {
+            paddle->count++;
         } else {
-            *cmd = C_UP;
-            *count = 1;
+            paddle->cmd = C_UP;
+            paddle->count = 1;
         }
-    } else if (digitalRead(down_pin)) {
-        if (*cmd == C_DOWN) {
-            (*count)++;
+    } else if (digitalRead(paddle->down_pin)) {
+        if (paddle->cmd == C_DOWN) {
+            paddle->count++;
         } else {
-            *cmd = C_DOWN;
-            *count = 1;
+            paddle->cmd = C_DOWN;
+            paddle->count = 1;
         }
     } else {
-        *cmd = C_NONE;      // so that old commands don't stack up
-        *count = 0;
+        paddle->cmd = C_NONE;      // so that old commands don't stack up
+        paddle->count = 0;
     }
 
-    if (*count == PADDLE_OVERFLOW) {
-        *count = 0;
-        if (*cmd == C_UP && *pos > 0)
-            (*pos)--;           // board y-axis grows down
-        else if (*cmd == C_DOWN && *pos + PADDLE_HEIGHT < BOARD_HEIGHT)
-            (*pos)++;
+    if (paddle->count == PADDLE_OVERFLOW) {
+        paddle->count = 0;
+        if (paddle->cmd == C_UP && paddle->y > 0) {
+            paddle->y--;           // board y-axis grows down
+        } else if (paddle->cmd == C_DOWN &&
+                   paddle->y + PADDLE_HEIGHT < BOARD_HEIGHT) {
+            paddle->y++;
+        }
     }
 
     draw_paddle(paddle);
 }
 
-void clear_paddle(uint8 paddle) {
-    uint8 px=0, py=0;           // shutup, compiler
-    switch (paddle) {
-    case LEFT_PADDLE:
-        px = LEFT_PADDLE_X;
-        py = pad_left;
-        break;
-    case RIGHT_PADDLE:
-        px = RIGHT_PADDLE_X;
-        py = pad_right;
-        break;
-    }
+void clear_paddle(paddle_t *paddle) {
     for (int y = 0; y < PADDLE_HEIGHT; y++) {
-        board[py+y][px] = OFF_COLOR;
+        board[paddle->y + y][paddle->x] = OFF_COLOR;
     }
 }
 
-void draw_paddle(uint8 paddle) {
-    uint8 px=0, py=0;           // shutup, compiler
-    switch (paddle) {
-    case LEFT_PADDLE:
-        px = LEFT_PADDLE_X;
-        py = pad_left;
-        break;
-    case RIGHT_PADDLE:
-        px = RIGHT_PADDLE_X;
-        py = pad_right;
-        break;
-    }
+void draw_paddle(paddle_t *paddle) {
     for (int y = 0; y < PADDLE_HEIGHT; y++) {
-        board[py+y][px] = ON_COLOR;
+        board[paddle->y + y][paddle->x] = ON_COLOR;
     }
 }
 
@@ -404,6 +430,7 @@ int main(void) {
     setup();
 
     while (1) {
+        // no loop -- everything happens in the interrupts
     }
 
     return 0;
