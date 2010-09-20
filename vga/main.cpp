f24798ae7acc7d92320ffb5a10415ad68df11912
Marti Bolivar <mbolivar@mit.edu>
2010-09-20 15:45:08 -0400

    bigger board

--- vga/main.cpp
+++ vga/main.cpp
@@ -18,7 +18,7 @@
 #define VGA_V 11    // STM32: A6
 #define VGA_H 12    // STM32: A7
 
-// These low level (and non-portable) macros make GPIO writes much faster
+// These low level macros make GPIO writes much faster
 #define ABSRR ((volatile uint32*)0x40010810)
 #define ABRR  ((volatile uint32*)0x40010814)
 
@@ -38,26 +38,17 @@
 #define VGA_BLUE   *ABSRR = BIT(BBIT) | BIT(RBIT+16) | BIT(GBIT+16)
 #define VGA_WHITE  *ABSRR = BIT(RBIT) | BIT(GBIT)    | BIT(BBIT)
 #define VGA_BLACK  *ABRR  = BIT(RBIT) | BIT(GBIT)    | BIT(BBIT)
-#define VGA_BIT(b) *ABSRR = BIT((b))  | BIT(RBIT+16) | BIT(GBIT+16) | BIT(BBIT+16) // set has priority!
+// set has priority, so clear every bit and set a given bit:
+#define VGA_BIT(b) (*ABSRR = BIT((b)) |                                \
+                    BIT(RBIT+16) | BIT(GBIT+16) | BIT(BBIT+16))
 
-#if 1 // confusing: changing between these has noticeable effects on timing
-#define VGA_V_HIGH (GPIOA_BASE)->BSRR = BIT(6)
-#define VGA_V_LOW  (GPIOA_BASE)->BRR  = BIT(6)
-#define VGA_H_HIGH (GPIOA_BASE)->BSRR = BIT(7)
-#define VGA_H_LOW  (GPIOA_BASE)->BRR  = BIT(7)
-#else
 #define VGA_V_HIGH *ABSRR = BIT(6)
 #define VGA_V_LOW  *ABRR  = BIT(6)
 #define VGA_H_HIGH *ABSRR = BIT(7)
 #define VGA_H_LOW  *ABRR  = BIT(7)
-#endif
 
-// pick one of these for when isr_start gets triggered, they'll place
-// the image appropriately
-#define START_IMAGE_FLUSH_LEFT 300
-#define START_IMAGE_CENTERED   600
-// we'll center
-#define START_IMAGE START_IMAGE_CENTERED
+// when in the 0-2286 cycle range we start drawing the board
+#define START_IMAGE 400
 
 // joystick pins
 #define LEFT_UP 20
@@ -65,16 +56,24 @@
 #define RIGHT_UP 17
 #define RIGHT_DOWN 16
 
+// controls the board size
+#define BOARD_WIDTH 54
+#define BOARD_HEIGHT 48
+
+// board is 1-bit, this controls the color of off/on
+#define ON_COLOR BBIT
+#define OFF_COLOR GBIT
+
 // paddle IDs
 #define LEFT_PADDLE 0
 #define RIGHT_PADDLE 1
 // paddle x positions, in board coordinates
 #define LEFT_PADDLE_X 0
-#define RIGHT_PADDLE_X 15
+#define RIGHT_PADDLE_X (BOARD_WIDTH-1)
 // controls how fast paddles move
 #define PADDLE_OVERFLOW 6
 // how big, in board pixels, to draw a paddle
-#define PADDLE_HEIGHT 4
+#define PADDLE_HEIGHT 7
 // controls how fast the ball moves
 #define BALL_OVERFLOW 3
 
@@ -86,7 +85,6 @@ typedef enum {
 // vga functions
 void isr_porch(void);
 void isr_start(void);
-void isr_stop(void);
 void isr_update(void);
 
 // board update functions
@@ -97,6 +95,9 @@ void draw_paddle(uint8);
 void update_ball();
 void clear_ball();
 void draw_ball();
+
+// this is (hackishly) used to force delays
+volatile uint8 volatile_int = 0;
 
 // vga state
 uint8 toggle;
@@ -115,35 +116,26 @@ int8 ball_x = 8, ball_y = 8;
 int8 ball_vx = 1, ball_vy = 1;
 uint8 ball_count = 0;
 
-// 1-bit!  this _must_ be 1-bit; an optimization depends on it.
+// 1-bit!
 //
 // access like board[y][x], where board[0][0] is upper left, and
-// board[17][15] is bottom right
-uint8 board[18][16] = {
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,},
-    {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,},
-    {0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,},
-    {1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,},
-    {0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,},
-    {1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,},
-    {0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,},
-    {1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,}, };
+// board[BOARD_HEIGHT-1][BOARD_WIDTH-1] is bottom right
+uint8 board[BOARD_HEIGHT][BOARD_WIDTH];
 uint8 *board_row;               // for speed
 
-#define BOARD_WIDTH 16
-#define BOARD_HEIGHT 18
-
 //------------------------------ setup()/loop() -------------------------------
+
+void setup_board() { // board cells with 1 become blue, 0 become green
+    for (int y = 0; y < BOARD_HEIGHT; y++) {
+        for (int x = 0; x < BOARD_WIDTH; x++) {
+            if (board[y][x]) {
+                board[y][x] = ON_COLOR;
+            } else {
+                board[y][x] = OFF_COLOR;
+            }
+        }
+    }
+}
 
 void setup() {
     // Setup our pins
@@ -163,9 +155,12 @@ void setup() {
     digitalWrite(VGA_H, HIGH);
     digitalWrite(VGA_V, HIGH);
 
-    // This gets rid of the majority of the interrupt artifacts
+    // This gets rid of interrupt artifacts
     SerialUSB.end();
     systick_disable();
+
+    // convert the board from binary into [RGB]BIT values
+    setup_board();
 
     // Configure
     Timer4.pause(); // while we configure
@@ -180,11 +175,13 @@ void setup() {
     Timer4.attachCompare1Interrupt(isr_porch);
     Timer4.setCompare2(START_IMAGE);
     Timer4.attachCompare2Interrupt(isr_start);
-    Timer4.setCompare3(2170);
-    Timer4.attachCompare3Interrupt(isr_stop);
+    // (we used to turn things black here, but now isr_start does it).
+    // if isr_start stops doing that, you may need to put this back in.
+    // Timer4.setCompare3(2170);
+    // Timer4.attachCompare3Interrupt(isr_stop);
     Timer4.setCompare4(1);      // Could be zero I guess
     Timer4.attachCompare4Interrupt(isr_update);
-    
+
     Timer4.setCount(0);         // Ready...
     Timer4.resume();            // Go!
 }
@@ -198,14 +195,14 @@ void setup() {
 void isr_porch(void) {
     VGA_H_HIGH;
     y++;
-    board_row = board[y/28];
+    board_row = board[y/(480/BOARD_HEIGHT)];
     // Back to the top
     if(y>=523) {
         y=1;
         v_active = 1;
         return;
     }
-    // Other vsync stuff below the image 
+    // Other vsync stuff below the image
     if(y>=492) {
         VGA_V_HIGH;
         return;
@@ -214,43 +211,45 @@ void isr_porch(void) {
         VGA_V_LOW;
         return;
     }
-    if(y==479) { 
+    if(y==479) {
         v_active = 0;           // stop drawing
         return;
     }
     // give lines 480--489 to game logic
-    if(y==480) {
+    if(y>=480) {
         advance_state();
         return;
     }
 }
 
 // This is the main horizontal sweep
-void isr_start(void) {  
+void isr_start(void) {
     // Skip if we're not in the image at all
     if(!v_active) { return; }
 
-    // Start red
+    // Start red.  Overhead setting up the main loop is going to make
+    // this appear as a wide band.
     VGA_RED;
 
-    for(x=0; x<16; x++) {
-        switch(board_row[x]) {
-        case 0:
-            VGA_GREEN;
-            break;
-        case 1:
-            VGA_BLUE;
-            break;
-        }
+    // main loop: draw the contents of the board as fast as we can.
+    // use VGA_BIT so per-iteration time is deterministic.
+    for(x=0; x < BOARD_WIDTH; x++) {
+        VGA_BIT(board_row[x]);
     }
 
-    // black out what's left
-    VGA_BLACK;
-}
+    // delay just a little so the last board pixel doesn't look so narrow
+    volatile_int++;
 
-// End of the horizontal line
-void isr_stop(void) {
-    if(!v_active) { return; }
+    // add another red band to mark off the edge
+    VGA_RED;
+    volatile_int++;
+    volatile_int++;
+    volatile_int++;
+    volatile_int++;
+    volatile_int++;
+    volatile_int++;
+
+    // black out what's left, or vsync won't work
     VGA_BLACK;
 }
 
@@ -261,64 +260,73 @@ void isr_update(void) {
 
 //-------------------------------- Game logic ---------------------------------
 
-// This function gets called at the end of a frame (i.e., after h-line
-// 479).  It's got until h-line 490 (i.e. 480--489 = 10 h-lines = 22870
-// cycles) to do its business, or weird timing bugs will manifest
+// the interrupt handlers will give lines 480--489 to this function,
+// which must finish its work before isr_stop on
 void advance_state() {
-#if 0
-    update_ball();
-#endif
-    // update_paddle(LEFT_PADDLE, LEFT_UP, LEFT_DOWN,
-    //               &pad_left, &left_cmd, &pad_left_count);
-    // update_paddle(RIGHT_PADDLE, RIGHT_UP, RIGHT_DOWN,
-    //               &pad_right, &right_cmd, &pad_right_count);
+    switch (y) {
+    case 480:
+        update_ball();
+        break;
+    case 481:
+        update_paddle(LEFT_PADDLE, LEFT_UP, LEFT_DOWN,
+                      &pad_left, &left_cmd, &pad_left_count);
+
+        break;
+    case 482:
+        update_paddle(RIGHT_PADDLE, RIGHT_UP, RIGHT_DOWN,
+                      &pad_right, &right_cmd, &pad_right_count);
+        break;
+    default:
+        break;
+    }
+    return;
 }
 
-inline void update_ball() {
+void update_ball() {
     clear_ball();
-    // ball_count++;
-    // if (ball_count >= BALL_OVERFLOW) {
-    //     ball_count = 0;
-    //     if (ball_vx > 0) {
-    //         if (ball_x + ball_vx < BOARD_WIDTH) {
-    //             ball_x += ball_vx;
-    //         } else {
-    //             ball_x = BOARD_WIDTH-1;
-    //             ball_vx *= -1;
-    //         }
-    //     } else {
-    //         if (ball_x + ball_vx > 0) {
-    //             ball_x += ball_vx;
-    //         } else {
-    //             ball_x = 0;
-    //             ball_vx *= -1;
-    //         }
-    //     }
-    //     if (ball_vy > 0) {
-    //         if (ball_y + ball_vy < BOARD_HEIGHT) {
-    //             ball_y += ball_vy;
-    //         } else {
-    //             ball_y = BOARD_HEIGHT-1;
-    //             ball_vy *= -1;
-    //         }
-    //     } else {
-    //         if (ball_y + ball_vy > 0) {
-    //             ball_y += ball_vy;
-    //         } else {
-    //             ball_y = 0;
-    //             ball_vy *= -1;
-    //         }
-    //     }
-    // }
+    ball_count++;
+    if (ball_count >= BALL_OVERFLOW) {
+        ball_count = 0;
+        if (ball_vx > 0) {
+            if (ball_x + ball_vx < BOARD_WIDTH) {
+                ball_x += ball_vx;
+            } else {
+                ball_x = BOARD_WIDTH-1;
+                ball_vx *= -1;
+            }
+        } else {
+            if (ball_x + ball_vx > 0) {
+                ball_x += ball_vx;
+            } else {
+                ball_x = 0;
+                ball_vx *= -1;
+            }
+        }
+        if (ball_vy > 0) {
+            if (ball_y + ball_vy < BOARD_HEIGHT) {
+                ball_y += ball_vy;
+            } else {
+                ball_y = BOARD_HEIGHT-1;
+                ball_vy *= -1;
+            }
+        } else {
+            if (ball_y + ball_vy > 0) {
+                ball_y += ball_vy;
+            } else {
+                ball_y = 0;
+                ball_vy *= -1;
+            }
+        }
+    }
     draw_ball();
 }
 
 inline void clear_ball() {
-    board[ball_y][ball_x] = 0;
+    board[ball_y][ball_x] = OFF_COLOR;
 }
 
 inline void draw_ball() {
-    board[ball_y][ball_x] = 1;
+    board[ball_y][ball_x] = ON_COLOR;
 }
 
 void update_paddle(uint8 paddle, uint8 up_pin, uint8 down_pin,
@@ -368,7 +376,7 @@ void clear_paddle(uint8 paddle) {
         break;
     }
     for (int y = 0; y < PADDLE_HEIGHT; y++) {
-        board[py+y][px] = 0;
+        board[py+y][px] = OFF_COLOR;
     }
 }
 
@@ -385,7 +393,7 @@ void draw_paddle(uint8 paddle) {
         break;
     }
     for (int y = 0; y < PADDLE_HEIGHT; y++) {
-        board[py+y][px] = 1;
+        board[py+y][px] = ON_COLOR;
     }
 }
 
