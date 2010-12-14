739c7fa74e514f52db0492ed98d8a71d615c84fc
Marti Bolivar <mbolivar@mit.edu>
2010-12-13 20:43:33 -0500

    Moved skeleton to use convenience functions

--- debug-serialusb/main.cpp
+++ debug-serialusb/main.cpp
@@ -7,9 +7,9 @@
  * tests should include:
  *    - usbSendBytes    - not dropping packets at any speed/size
  *    - usbReceiveBytes - not dropping packets at any speed/size
- * 
+ *
  *    - SerialUSB.print  - blocking write, always works
- *    - SerialUSB.read() - blocking read, returns one byte in sequence and drops none 
+ *    - SerialUSB.read() - blocking read, returns one byte in sequence and drops none
  *
  * This code is released into the public domain.
  */
@@ -17,22 +17,15 @@
 #include "wirish.h"
 #include "usb.h"
 
-
-//------------------------------ program macros -------------------------------
-#define LED_PIN (13)
-
 //------------------------------ setup()/loop() -------------------------------
 
-
 void setup() {
-    pinMode(LED_PIN,OUTPUT);
+    pinMode(BOARD_LED_PIN, OUTPUT);
 }
 
 void loop() {
-    static int  toggle = 0;
-
-    toggle ^= 1;
-    digitalWrite(LED_PIN,toggle);
+    toggleLED();
+    delay(500);
 }
 
 
