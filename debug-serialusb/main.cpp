062869de7633a7271a95d03ceb6a14b4003b1a14
Marti Bolivar <mbolivar@mit.edu>
2010-12-13 22:40:15 -0500

    adding test rig for usbSendBytes()/usbReceiveBytes()

--- debug-serialusb/main.cpp
+++ debug-serialusb/main.cpp
@@ -17,17 +17,87 @@
 #include "wirish.h"
 #include "usb.h"
 
-//------------------------------ setup()/loop() -------------------------------
+#define ERROR_CHAR ('*')
+#define CLEAR_CHAR ('_')
 
 void setup() {
     pinMode(BOARD_LED_PIN, OUTPUT);
 }
 
-void loop() {
-    toggleLED();
-    delay(500);
+uint8 choose_fill(int len) {
+    switch (len) {
+    case 1:
+        return'a';
+    case 2:
+        return'b';
+    case 4:
+        return'c';
+    case 8:
+        return'd';
+    case 16:
+        return'e';
+    case 32:
+        return'f';
+    case 64:
+        return'g';
+    case 128:
+        return'h';
+    default:
+        return 'X';
+    }
 }
 
+void fill_buf(uint8 *buf, int len, uint8 c) {
+    for (int i = 0; i < len; i++) buf[i] = c;
+}
+
+void send_buf(uint8 *buf, int len) {
+    uint8 sent = 0;
+    while (sent < len) {
+        int n = usbSendBytes(buf + sent, len - sent);
+        sent += n;
+    }
+}
+
+void recv_buf(uint8 *buf, int len) {
+    uint8 recvd = 0;
+    uint8 expected = choose_fill(len);
+    while (recvd < len) {
+        int n = usbReceiveBytes(buf + recvd, len - recvd);
+        for (int i = 0; i < n; i++) {
+            if (buf[recvd + i] != expected) buf[recvd + i] = ERROR_CHAR;
+        }
+        recvd += n;
+        toggleLED();
+    }
+}
+
+void loop() {
+    static uint8 buf[128];
+    static int len = 1;
+    uint8 fill = choose_fill(len);
+
+    // let the host know what we think is about to happen
+    waitForButtonPress(0);
+    SerialUSB.print("size = ");
+    SerialUSB.print(len);
+    SerialUSB.print(", fill = ");
+    SerialUSB.println(fill);
+
+    // clear out last run, then fill with correct char for current len
+    fill_buf(buf, len, fill);
+
+    // round-trip the buffer once, marking places where received
+    // wasn't expected
+    send_buf(buf, len);
+    recv_buf(buf, len);
+
+    // send it again, with errors marked
+    send_buf(buf, len);
+
+    len *= 2;
+    if (len > 128) len = 1;
+}
 
 // Force init to be called *first*, i.e. before static object allocation.
 // Otherwise, statically allocated object that need libmaple may fail.
---
+++ debug-serialusb/serial_echo.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+
+import optparse
+import sys
+
+import serial
+
+BAUD = 9600
+MAX_PACKET_SIZE = 128
+ERROR_CHAR = '*'
+
+def serial_echo(port, baudrate=BAUD):
+    ser = serial.Serial(port, baudrate=baudrate)
+
+
+    size = 1
+    while True:
+        print('Handshake: "{0}"'.format(ser.readline().strip()))
+        print('size = {0}'.format(size))
+        s = ser.read(size)
+        print('read packet: "{1}"'.format(len(s), s))
+        ser.write(s)
+        print('wrote packet back.')
+        s = ser.read(size)
+        print('read packet response: "{1}"'.format(len(s), s))
+        if ERROR_CHAR in s: print('-- errors detected.')
+
+        print()
+
+        size *= 2
+        if size > MAX_PACKET_SIZE:
+            size = 1
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser(usage='usage: %prog [options] port')
+    parser.add_option('-b', '--baud-rate', default=BAUD,
+                      help='Baud rate (Kbps)')
+
+    opts, args = parser.parse_args()
+
+    def usage(msg, fatal=True):
+        if fatal: print ('Error: ', end='')
+        print(msg)
+        parser.print_help()
+        if fatal: sys.exit(1)
+
+    if len(args) != 1:
+        usage('Must specify exactly one serial port.')
+
+    port = args[0]
+
+    print('Using port {0}'.format(port))
+
+    serial_echo(port, baudrate=opts.baud_rate)
