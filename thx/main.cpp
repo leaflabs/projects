9ec98c047064ef12c63dd80ee782ebefdcaaea53
AJM <poslathian@poslathian.(none)>
2011-01-31 02:07:46 -0500

    added a sine table, 16 bit 4096 samples

--- thx/main.cpp
+++ thx/main.cpp
@@ -4,19 +4,21 @@
 
 #include "wirish.h"
 #include "usb.h"
+//#include "sine.c"
 
 #define BOARD_LED_PIN 13
 #define BOARD_BUT_PIN 38
 
+int32 current_sample = 0; /* 32 bits in case we use fixed point later */
 
-void setup() {
+void setup(void) {
     pinMode(BOARD_BUT_PIN,INPUT);
     pinMode(BOARD_LED_PIN,OUTPUT);
     while (!digitalRead(BOARD_BUT_PIN)); // wait for press
 
 }
 
-void loop() {
+void loop(void) {
     digitalWrite(BOARD_LED_PIN,HIGH);
     delay(1000);
     digitalWrite(BOARD_LED_PIN,LOW);
