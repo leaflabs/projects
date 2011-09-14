e8401a7283b67f983c38b474539d4d39b645723b
Dave McCoy <cospan@gmail.com>
2011-09-14 09:58:19 -0400

    fixed the Makefile, not instead of clobbering 'CFLAGS' I use 'ALL_CFLAGS' which is something that pmvm's Makefile utilizes
    fixed main.cpp to use the maple version of pmvm
    
    copied syscalls.c which basically is a front end to the serial port "file" handling all the functions like read, write, etc
    
    removed update_periph_lib.py which is from the stm32 port that is a python script for the desktop to pull in libraries for stm32 off the net

--- pmvm/src/platform/maple/main.cpp
+++ pmvm/src/platform/maple/main.cpp
@@ -13,7 +13,8 @@
 */
 
 #include "wirish.h"
-//#include "pm.h"
+#include "pmvm.h"
+#include "../../vm/pm.h"
 
 extern unsigned char usrlib_img[];
 // Force init to be called *first*, i.e. before static object allocation.
@@ -23,20 +24,17 @@ __attribute__((constructor)) void premain() {
 }
 
 
-int
-main(void)
-{
-/*
+int main(void){
+
+
     PmReturn_t retval;
+	//initialize everything behind pmvm
+	PMVM pmvm = PMVM();
 
-    // Init PyMite
-    retval = pm_init(MEMSPACE_PROG, usrlib_img);
-    PM_RETURN_IF_ERROR(retval);
+	//let pm initialize everything
+	retval = pm_init(MEMSPACE_PROG, usrlib_img);
+	PM_RETURN_IF_ERROR(retval);
 
-    // Run the sample program
-    retval = pm_run((uint8_t *)"main");
-
-
-    return (int)retval;
-	*/
+	retval = pm_run((uint8_t *) "main");
+	return (int) retval;
 }
