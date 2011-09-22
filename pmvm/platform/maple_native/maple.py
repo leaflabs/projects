8e9f5152ab55ba21089a4da6cfa201505b59f741
Andrew J Meyer <ajm@leaflabs.com>
2011-09-22 14:25:27 -0400

    added a missing pass statement to the maple ffi test

--- pmvm/platform/maple_native/maple.py
+++ pmvm/platform/maple_native/maple.py
@@ -5,29 +5,30 @@
 """
 
 def init_gpio():
-	"""__NATIVE__
-	PmReturn_t retval = PM_RET_OK;
-	gpio_init(GPIOC);
-	gpio_set_mode(GPIOC, 15, GPIO_OUTPUT_PP);
-	NATIVE_SET_TOS(PM_NONE);
-	return retval;
-	"""
-	pass
+        """__NATIVE__
+        PmReturn_t retval = PM_RET_OK;
+        gpio_init(GPIOC);
+        gpio_set_mode(GPIOC, 15, GPIO_OUTPUT_PP);
+        NATIVE_SET_TOS(PM_NONE);
+        return retval;
+        """
+        pass
 
 def toggle_led():
-	"""__NATIVE__
-	PmReturn_t retval = PM_RET_OK;
-	gpio_toggle_bit(GPIOC, 15);
+        """__NATIVE__
+        PmReturn_t retval = PM_RET_OK;
+        gpio_toggle_bit(GPIOC, 15);
     NATIVE_SET_TOS(PM_NONE);
-	return retval;
-	"""
-	pass
+        return retval;
+        """
+        pass
 
 def test():
-	"""__NATIVE__
-	PmReturn_t retval = PM_RET_OK;
-	NATIVE_SET_TOS(PM_NONE);
-	return retval;
-	"""
+        """__NATIVE__
+        PmReturn_t retval = PM_RET_OK;
+        NATIVE_SET_TOS(PM_NONE);
+        return retval;
+        """
+        pass
 
 # :mode=c:
